0b1ea505ce5aaf0a58ba7f3bb7f89a1af7e307e4
Dipak Rathod <dipakrathod258@gmail.com>
2019-11-08 11:53:14 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Response;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+
+    public function register_credentials(Request $request){
+        // dd($request->all());
+        $data=User::find($request->userId);
+        $data->authSecret = $request->authSecret;
+        $data->key = $request->key;
+        $data->endpoint = $request->endpoint;
+        $data->save();
+        return Response::json(array('success'=>'true'));
+    }
+}
